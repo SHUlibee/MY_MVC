8e4ab2994c61dffbf3e5f05e39bc4f662910dacb
libiying <libiying@ihuilian.com>
2015-01-26 16:41:39 +0800

    单例设计模式，每个model只能实例化一次

--- libs/loader.php
+++ libs/loader.php
@@ -4,19 +4,25 @@
  * @author bee
  */
 class Loader_Lib{
-	
+
+    private static $load;
+
 	public function __construct(){
 		
 	}
 
     /**
+     * 单例设计模式，保证model只被实例化一次
      * @param $model
      * @return Xxx_Model
      */
     public function model($model){
 
         $model = $model.'_Model';
-        return new $model();
+        if(!isset(self::$load[$model])){
+            self::$load[$model] = new $model();
+        }
+        return self::$load[$model];
     }
 
 }
